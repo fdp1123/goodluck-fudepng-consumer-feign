dcc15318686648b18f0ff3b8ab4f0268950d73f2
fudepeng <491031768@qq.com>
2017-07-28 18:42:45 +0800

    Initial commit

---
+++ src/main/java/com/bw/fudepng/GoodluckFudepngConsumerFeignApplication.java
@@ -0,0 +1,12 @@
+package com.bw.fudepng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodluckFudepngConsumerFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckFudepngConsumerFeignApplication.class, args);
+	}
+}
